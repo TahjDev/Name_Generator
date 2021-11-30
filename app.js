42761c99f8112480113e3eaf72a2f1ec1d203475
tahj <tahjharris00@gmail.com>
2021-11-29 21:29:26 -0600

    server up and running

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express();
 const port = 3000;
 
 
-app.get("/", (req, resp) => resp.send("Helooo World"))
+app.get("/", (req, resp) => resp.send("Helooo World"));
 
 app.listen(port, () => {
-    console.log("Example app listenting")
-})
+    console.log("Example app listenting");
+});
+
+app.get("/users/:name", (req, resp) => {
+    const obj = {}
+    obj["birthday"] = req.query["birthday"]
+    obj["name"] = req.params["name"]
+    resp.send(obj)
+});
